490cb24915d859d6428331a6e7551e6896563223
Matthias Fey <matthias.fey@tu-dortmund.de>
2016-01-29 16:25:55 +0100

    added koa router with test api call

--- server/index.js
+++ server/index.js
@@ -2,13 +2,27 @@
 
 import Koa from 'koa';
 import logger from 'koa-logger';
+import helmet from 'koa-helmet';
+import Router from 'koa-router';
 
 const app = new Koa();
 
 app.use(logger());
+app.use(helmet());  // set HTTP headers (e.g. remove x-powered-by)
+
+const router = new Router({
+  prefix: '/api',
+});
+
+router.get('/', function (ctx, next) {
+  ctx.body = { message: 'api call' };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.use(async ctx => {
-  ctx.body = { message: 'hello world' };
+  ctx.status = 404;
 });
 
 export default app;
