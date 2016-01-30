843f2d1c02f7fe05c834e920532f83bc30d6c7a6
Matthias Fey <matthias.fey@tu-dortmund.de>
2016-01-30 17:22:24 +0100

    added convert module to support koa1-middleware

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 'use strict';
 
 import Koa from 'koa';
+import convert from 'koa-convert';
 import logger from 'koa-logger';
 import helmet from 'koa-helmet';
+import cors from 'koa-cors';
+import bodyParser from 'koa-bodyparser';
 import Router from 'koa-router';
 
 const app = new Koa();
 
+// convert legacy generator to promise middleware
+const _use = app.use;
+app.use = x => _use.call(app, convert(x));
+
 app.use(logger());
 app.use(helmet());  // set HTTP headers (e.g. remove x-powered-by)
+app.use(cors()); // enable cors
+app.use(bodyParser());
 
 const router = new Router({
   prefix: '/api',
